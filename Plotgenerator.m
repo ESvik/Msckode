b4153881a051bd0975611e3e3073c4466255f57b
Erlend <erlend.storvik@gmail.com>
2018-08-14 11:24:39 +0100

    MAndelP1

--- Plotgenerator.m
+++ Plotgenerator.m
@@ -7,7 +7,7 @@ p3 = plot(Analysis(1:end-1,5),Analysis(1:end-1,6),'--o','MarkerSize',10,'MarkerI
 p4 = plot(Analysis(1:end-1,7),Analysis(1:end-1,8),'.:','MarkerSize',10)
 p5 = plot(Analysis(1:end-1,9),Analysis(1:end-1,10),'-.','MarkerSize',10)
 %p6 = plot(Analysis(1:end-1,11),Analysis(1:end-1,12),'--')
-p7 = plot(Analysis(end,1:2:11),Analysis(end,2:2:12),'p','MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
+p7 = plot(Analysis(end,1:2:9),Analysis(end,2:2:10),'p','MarkerEdgeColor','k','MarkerFaceColor','k','MarkerSize',10)
 hold off
 legend([p1,p2,p3,p4,p5],'\kappa = 10^{-14}','\kappa = 10^{-13}','\kappa = 10^{-12}','\kappa = 10^{-11}','\kappa = 10^{-10}')
 xlabel('\delta')
